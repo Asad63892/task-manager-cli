401794e8b5e8174410e2a180e9a02e306f533483
Asad <asasadkhan8@gmail.com>
2025-12-25 12:42:00 +0500

    Add base TaskManager CLI code

---
+++ src/TaskManager.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskManager {
+    static ArrayList<String> tasks = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("\n--- Task Manager CLI ---");
+            System.out.println("1. Add Task");
+            System.out.println("2. Show Tasks");
+            System.out.println("3. Delete Task");
+            System.out.println("4. Exit");
+            System.out.print("Choose: ");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    addTask();
+                    break;
+                case 2:
+                    showTasks();
+                    break;
+                case 3:
+                    deleteTask();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+    }
+
+    // Features will be implemented by team members
+    static void addTask() {}
+    static void showTasks() {}
+    static void deleteTask() {}
+}
